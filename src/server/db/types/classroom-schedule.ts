ab750a985c39e2110ada14988765a493b808cdaa
Darvey Trinidad <trinidaddarvey10@gmail.com>
2025-07-19 00:18:57 +0800

    feat(classroom-sched): add db types for inserting and editing records

---
+++ src/server/db/types/classroom-schedule.ts
@@ -0,0 +1,10 @@
+import { classroomSchedule, classroomVacancy, classroomBorrowing } from "@/server/db/schema/classroom-schedule";
+import { type InferInsertModel } from "drizzle-orm";
+
+export type ClassroomSchedule = InferInsertModel<typeof classroomSchedule>;
+export type ClassroomVacancy = InferInsertModel<typeof classroomVacancy>;
+export type ClassroomBorrowing = InferInsertModel<typeof classroomBorrowing>;
+
+export type EditClassroomSchedule = { id: string } & Partial<Omit<ClassroomSchedule, "id">>;
+export type EditClassroomVacancy = { id: string } & Partial<Omit<ClassroomVacancy, "id">>;
+export type EditClassroomBorrowing = { id: string } & Partial<Omit<ClassroomBorrowing, "id">>;  
