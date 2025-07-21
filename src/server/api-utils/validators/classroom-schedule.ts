240c9aee1345a8da16147946ceb4a68e3185ff6b
Darvey Trinidad <trinidaddarvey10@gmail.com>
2025-07-21 23:47:02 +0800

    add zod schema for creating classroom schedule

---
+++ src/server/api-utils/validators/classroom-schedule.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+import { DAYS } from "@/constants/days";
+import { timeIntSchema } from "@/constants/timeslot";
+
+export const createClassroomScheduleSchema = z.object({
+  classroomId: z.string(),
+  facultyId: z.string(),
+
+  day: z.enum(DAYS),
+  startTime: timeIntSchema,
+  endTime: timeIntSchema,
+
+  subject: z.string(),
+  section: z.string(),
+}).refine(
+  (data) => {
+    return data.endTime > data.startTime;
+  },
+  {
+    message: "End time must be later than start time",
+    path: ["endTime"],
+  }
+);
+
+export type CreateClassroomScheduleInput = z.infer<typeof createClassroomScheduleSchema>;
