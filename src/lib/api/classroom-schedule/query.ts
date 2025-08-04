8eee31ddcf28e732ff5c95535daf206635c5d82a
Darvey Trinidad <trinidaddarvey10@gmail.com>
2025-08-04 23:31:44 +0800

    feat(classroom-sched): add getClassroomBorrowingConflicts query

--- src/lib/api/classroom-schedule/query.ts
+++ src/lib/api/classroom-schedule/query.ts
@@ -1,6 +1,6 @@
 import { db, and, eq, inArray } from "@/server/db";
-import { classroomSchedule, classroomVacancy } from "@/server/db/schema/classroom-schedule";
-import type { ClassroomScheduleWithoutId, ClassroomVacancyWithoutId } from "@/server/db/types/classroom-schedule";
+import { classroomSchedule, classroomVacancy, classroomBorrowing } from "@/server/db/schema/classroom-schedule";
+import type { ClassroomScheduleWithoutId, ClassroomVacancyWithoutId, ClassroomBorrowingWithoutId } from "@/server/db/types/classroom-schedule";
 import type { TimeInt } from "@/constants/timeslot";
 
 export const getClassroomScheduleConflicts = async (newSchedule: ClassroomScheduleWithoutId, startTimes: TimeInt[]) => {
@@ -31,6 +31,23 @@ export const getClassroomVacancyConflicts = async (newVacancy: ClassroomVacancyW
           eq(classroomVacancy.date, newVacancy.date),
           eq(classroomVacancy.classroomId, newVacancy.classroomId),
           inArray(classroomVacancy.startTime, startTimes)
+        )
+      );
+  } catch (error) {
+    console.log("Failed to get classroom vacancy conflicts:", error);
+    throw new Error("Could not get classroom vacancy conflicts");
+  }
+}
+export const getClassroomBorrowingConflicts = async (newBorrowing: ClassroomBorrowingWithoutId, startTimes: TimeInt[]) => {
+  try {
+    return await db
+      .select()
+      .from(classroomBorrowing)
+      .where(
+        and(
+          eq(classroomBorrowing.date, newBorrowing.date),
+          eq(classroomBorrowing.classroomId, newBorrowing.classroomId),
+          inArray(classroomBorrowing.startTime, startTimes)
         )
       );
   } catch (error) {
