d8e9b8ebdeeb6250178291d5aa15ad7ba9ae9481
Dochiibells <villarcarlogian14@gmail.com>
2025-08-17 13:49:47 +0800

    feat(table): added table folder in components fo reusability

--- src/app/(app)/users/_components/columns.tsx
+++ src/app/(app)/users/_components/columns.tsx
@@ -1,1 +1,35 @@
 "use clients";
+
+import { type ColumnDef } from "@tanstack/react-table";
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  role: string;
+  departmentOrOrganization: string;
+  isActive: boolean;
+}
+
+export const columns: ColumnDef<User>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "role",
+    header: "Role",
+  },
+  {
+    accessorKey: "departmentOrOrganization",
+    header: "Department/Organization",
+  },
+  {
+    accessorKey: "isActive",
+    header: "Status",
+  },
+];
