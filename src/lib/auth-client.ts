e182f5bacbf3c4e8009bcc4fb5bebd1506159198
Darvey Trinidad <trinidaddarvey10@gmail.com>
2025-08-31 17:01:18 +0800

    feat(auth): add session type on authclient

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,13 @@
 import { env } from "@/env";
 import { createAuthClient } from "better-auth/react";
+import { inferAdditionalFields } from "better-auth/client/plugins";
+import { auth } from "@/lib/auth";
 
 export const authClient = createAuthClient({
   baseURL: env.NEXT_PUBLIC_BETTER_AUTH_URL ?? "http://localhost:3000",
+  plugins: [
+    inferAdditionalFields<typeof auth>(),
+  ]
 });
+
+export type BetterAuthSession = typeof authClient.$Infer.Session;
