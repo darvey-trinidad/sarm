ec81975a0828648ffbbd0770b3979cca6fb9e759
Dochiibells <villarcarlogian14@gmail.com>
2025-09-09 23:20:22 +0800

    feat(dynamic social hall & activity-center):fetch from the database, no longer static it is dynamicly being displayed

---
+++ src/app/(app)/schedule/_components/venue-card.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import { api } from "@/trpc/react";
+
+type Venue = {
+  id: string;
+  name: string;
+};
+export default function VenueCard() {
+  const { data: venues } = api.venue.getAllVenues.useQuery();
+  return (
+    <div className="flex flex-col gap-5 sm:flex-row">
+      {venues?.map((venue) => (
+        <Card
+          key={venue.id}
+          className="h-85 w-85 cursor-pointer bg-white shadow-md transition-shadow duration-300 hover:shadow-xl"
+        >
+          <CardContent className="flex h-full flex-col p-0">
+            {/* Header */}
+            <div className="flex items-center gap-2 pb-2 pl-4">
+              <div className="h-4 w-4 rounded-sm bg-orange-500"></div>
+              <h3 className="text-medium font-semibold text-gray-800">
+                {venue.name}
+              </h3>
+            </div>
+
+            {/* Illustration */}
+            <div className="flex flex-1 items-center justify-center p-2 pt-0">
+              <div className="relative h-full max-h-64 w-full max-w-64">
+                <Image
+                  src={"/social-hall-card.png"}
+                  alt={venue.name}
+                  layout="responsive"
+                  objectFit="contain"
+                  width={500}
+                  height={500}
+                />
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+}
--- src/app/(app)/schedule/page.tsx
+++ src/app/(app)/schedule/page.tsx
@@ -1,13 +1,18 @@
 import { type Metadata } from "next";
 import BreadcrumbLayout from "@/components/breadcrumb/page-breadcrumb";
 import ClassroomCard from "./_components/classroom-card";
-import ActivityCenterCard from "./_components/activity-center-card";
-import SocialHallCard from "./_components/social-hall-card";
 import { PageRoutes } from "@/constants/page-routes";
 import Link from "next/link";
+import { api } from "@/trpc/react";
+import VenueCard from "./_components/venue-card";
 
 export const metadata: Metadata = {
   title: "Schedule",
+};
+
+type Venue = {
+  id: string;
+  name: string;
 };
 
 export default function Page() {
@@ -18,12 +23,7 @@ export default function Page() {
         <Link href={PageRoutes.SCHEDULE_CLASSROOM}>
           <ClassroomCard />
         </Link>
-        <Link href={PageRoutes.SCHEDULE_ACTIVITY_CENTER}>
-          <ActivityCenterCard />
-        </Link>
-        <Link href={PageRoutes.SCHEDULE_SOCIAL_HALL}>
-          <SocialHallCard />
-        </Link>
+        <VenueCard />
       </div>
     </div>
   );
