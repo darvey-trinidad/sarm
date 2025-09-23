a0decb600a6a0aedf25f54001693cce52b3d107b
Dochiibells <villarcarlogian14@gmail.com>
2025-09-23 14:23:08 +0800

    feat(reservation): added venue-reservation for viewing and managing venue request

--- src/app/(app)/requests/page.tsx
+++ src/app/(app)/requests/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { type Metadata } from "next";
 import BreadcrumbLayout from "@/components/breadcrumb/page-breadcrumb";
-
+import VenueReservation from "./_components/venue-reservation";
 export const metadata: Metadata = {
   title: "Requests",
 };
@@ -10,6 +10,8 @@ const Requests = () => {
   return (
     <div className="flex w-full flex-col space-y-4">
       <BreadcrumbLayout currentPage="Requests" />
+
+      <VenueReservation />
     </div>
   );
 };
