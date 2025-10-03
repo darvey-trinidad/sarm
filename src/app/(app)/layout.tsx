b1d855d278c72ed13ed515db04196ec68ba18f8e
Darvey Trinidad <trinidaddarvey10@gmail.com>
2025-10-03 19:05:52 +0800

    feat(auth): redirect users with no session to login

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,12 +1,23 @@
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { PageRoutes } from "@/constants/page-routes";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect(PageRoutes.LOGIN);
+  }
+
   return (
     <SidebarProvider>
       <AppSidebar />
--- src/middleware.ts
+++ src/middleware.ts
@@ -16,5 +16,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-	matcher: ["/", "/schedule"], // Specify the routes the middleware applies to
+	matcher: [], // Specify the routes the middleware applies to
 };
