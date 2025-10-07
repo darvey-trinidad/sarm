a3991814a0835707b8b0ef6f0b46b47ab99dbca8
Dochiibells <villarcarlogian14@gmail.com>
2025-10-07 23:17:01 +0800

    ref(page): added a new page for user profile

---
+++ src/app/(app)/profile/_components/user-profile.tsx
@@ -0,0 +1,7 @@
+export default function UserProfile() {
+  return (
+    <div>
+      <h1>hello this is user profile</h1>
+    </div>
+  );
+}
--- src/components/sidebar/user-sidebar.tsx
+++ src/components/sidebar/user-sidebar.tsx
@@ -1,8 +1,5 @@
 "use client";
-import {
-  LogOut,
-  CircleUserRound,
-} from "lucide-react";
+import { LogOut, CircleUserRound } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
@@ -15,19 +12,23 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { PageRoutes } from "@/constants/page-routes";
 
 export const UserSidebar = () => {
   const router = useRouter();
   const { data: session, isPending } = authClient.useSession();
 
+  const handleOpenProfile = () => {
+    window.location.href = `${process.env.NEXT_PUBLIC_APP_URL}${PageRoutes.USER_PROFILE}`;
+  };
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <button className="flex flex-col items-center justify-center gap-1.5 py-3 px-2 rounded-lg transition-colors duration-200 text-gray-600 hover:bg-gray-100 hover:text-gray-900 w-full">
+        <button className="flex w-full flex-col items-center justify-center gap-1.5 rounded-lg px-2 py-3 text-gray-600 transition-colors duration-200 hover:bg-gray-100 hover:text-gray-900">
           <Avatar className="h-8 w-8">
             <CircleUserRound className="h-8 w-8 stroke-1" />
           </Avatar>
-          <span className="text-xs font-medium text-center leading-tight">
+          <span className="text-center text-xs leading-tight font-medium">
             Profile
           </span>
         </button>
@@ -39,7 +40,10 @@ export const UserSidebar = () => {
         sideOffset={8}
       >
         <DropdownMenuLabel className="p-0 font-normal">
-          <div className="flex items-center gap-2 px-2 py-2 text-left text-sm">
+          <div
+            className="flex items-center gap-2 px-2 py-2 text-left text-sm hover:cursor-pointer"
+            onClick={handleOpenProfile}
+          >
             <Avatar className="h-8 w-8 rounded-lg">
               <CircleUserRound className="h-8 w-8 stroke-1" />
             </Avatar>
