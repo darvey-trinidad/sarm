f56b7677b2f50bf2bea662df1efff5ea5f07597e
Dochiibells <villarcarlogian14@gmail.com>
2025-10-08 02:13:22 +0800

    feat(user-profile): added the user details and made it editable

--- src/app/(app)/profile/page.tsx
+++ src/app/(app)/profile/page.tsx
@@ -1,15 +1,22 @@
-"use client";
 import React from "react";
+import { type Metadata } from "next";
 import BreadcrumbLayout from "@/components/breadcrumb/page-breadcrumb";
 import UserProfile from "./_components/user-profile";
-import { authClient } from "@/lib/auth-client";
 
+export const metadata: Metadata = {
+  title: "Profile",
+};
 const Profile = () => {
-  const { data: userProfile } = authClient.useSession();
-
   return (
     <div className="flex w-full flex-col space-y-4">
       <BreadcrumbLayout currentPage="Profile" />
+      <div className="flex flex-col">
+        <h2 className="text-2xl font-bold">User Profile</h2>
+        <p className="text-muted-foreground">
+          View and manage your personal information.
+        </p>
+      </div>
+      <UserProfile />
     </div>
   );
 };
